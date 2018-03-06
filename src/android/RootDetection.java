1e0840728649c56c46efb4ef091c3f20833fe9cc
liam.ryan <liamryandev@gmail.com>
2018-03-06 16:19:15 +0000

    Added convoluted logic to make .smali harder to trace

--- src/android/RootDetection.java
+++ src/android/RootDetection.java
@@ -1,54 +1,61 @@
 package ru.trykov.root;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.lang.Exception;
 import java.io.File;
 
 /**
  * Detect weather device is rooted or not
+ *
  * @author trykov
  */
 public class RootDetection extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("isDeviceRooted")) {
-            try {
-                callbackContext.success(isDeviceRooted() ? 1 : 0);
-                return true;
-            } catch (Exception e) {
-                callbackContext.error("N/A");
-                return false;
-            }
-        }
-        return false;
-    }
+	@Override
+	public boolean execute( String action, JSONArray args, CallbackContext callbackContext ) throws JSONException {
+		if ( action.equals( "check" ) ) {
+			try {
+				callbackContext.success( getStatus() );
+				return true;
+			} catch ( Exception e ) {
+				callbackContext.error( "N/A" );
+				return false;
+			}
+		}
+		return false;
+	}
 
-    private boolean isDeviceRooted() {
-        return checkBuildTags() || checkSuperUserApk() || checkFilePath();
-    }
-    private boolean checkBuildTags() {
-        String buildTags = android.os.Build.TAGS;
-        return buildTags != null && buildTags.contains("test-keys");
-    }
+	private int isCompromised() {
+		if ( checkBuildTags() || checkSuperUserApk() || checkFilePath() ) {
+			return new String( "iAws" ).compareTo( new String( "iAwr" ) );
+		}
+		return new String( "iAws" ).compareTo( new String( "iAws" ) );
+	}
 
-    private boolean checkSuperUserApk() {
-        return new File("/system/app/Superuser.apk").exists();
-    }
+	private int getStatus() {
+		return isCompromised();
+	}
 
-    private boolean checkFilePath() {
-        String[] paths = { "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
-                "/system/bin/failsafe/su", "/data/local/su" };
-        for (String path : paths) {
-            if (new File(path).exists()) return true;
-        }
-        return false;
-    }
+	private boolean checkBuildTags() {
+		String buildTags = android.os.Build.TAGS;
+		return buildTags != null && buildTags.contains( "test-keys" );
+	}
+
+	private boolean checkSuperUserApk() {
+		return new File( "/system/app/Superuser.apk" ).exists();
+	}
+
+	private boolean checkFilePath() {
+		String[] paths = { "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su", "/system/bin/failsafe/su", "/data/local/su" };
+		for ( String path : paths ) {
+			if ( new File( path ).exists() ) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
